65eb6089691af902b8e16856be7498572329efde
UooUzzz <uuumonazzz@gmail.com>
2024-12-26 15:56:27 +0900

    fix: detailPage start

--- lib/pages/detail/detail_page.dart
+++ lib/pages/detail/detail_page.dart
@@ -4,15 +4,10 @@ class DetailPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.separated(
-        itemCount: 5,
-        itemBuilder: (context, index) {
-          return Container();
-        },
-        separatorBuilder: (context, index) {
-          return Divider();
-        },
-        
+      body: ListView(
+        children: [
+          Container(),
+        ],
       ),
     );
   }
