a51c1161ef71862abd829b185cec3ef269d6af9a
UooUzzz <uuumonazzz@gmail.com>
2024-12-27 21:16:25 +0900

    add: movie_repository_imple

--- lib/data/repository/movie_repository_impl.dart
+++ lib/data/repository/movie_repository_impl.dart
@@ -9,10 +9,10 @@ class MovieRepositoryImpl implements MovieRepository {
   final MovieDataSource _movieDataSource;
 
   @override
-  Future<List<Movie>?> fetchNowPlayingMovies() async {
+  Future<List<Movie>> fetchNowPlayingMovies() async {
     final result = await _movieDataSource.fetchNowPlayMovies();
     return result
-        ?.map(
+        .map(
           (e) => Movie(
             id: e.id,
             popularity: e.popularity,
@@ -23,26 +23,65 @@ class MovieRepositoryImpl implements MovieRepository {
   }
 
   @override
-  Future<List<Movie>?> fetchPopularMovies() {
-    // TODO: implement fetchPopularMovies
-    throw UnimplementedError();
+  Future<List<Movie>> fetchPopularMovies() async {
+    final result = await _movieDataSource.fetchPopularMovies();
+    return result
+        .map(
+          (e) => Movie(
+            id: e.id,
+            popularity: e.popularity,
+            posterPath: e.posterPath,
+          ),
+        )
+        .toList();
   }
 
   @override
-  Future<List<Movie>?> fetchTopRatedMovies() {
-    // TODO: implement fetchTopRatedMovies
-    throw UnimplementedError();
+  Future<List<Movie>> fetchTopRatedMovies() async {
+    final result = await _movieDataSource.fetchTopRatedMovies();
+    return result
+        .map(
+          (e) => Movie(
+            id: e.id,
+            popularity: e.popularity,
+            posterPath: e.posterPath,
+          ),
+        )
+        .toList();
   }
 
   @override
-  Future<List<Movie>?> fetchUpcomingMovies() {
-    // TODO: implement fetchUpcomingMovies
-    throw UnimplementedError();
+  Future<List<Movie>> fetchUpcomingMovies() async {
+    final result = await _movieDataSource.fetchUpcomingMovies();
+    return result
+        .map(
+          (e) => Movie(
+            id: e.id,
+            popularity: e.popularity,
+            posterPath: e.posterPath,
+          ),
+        )
+        .toList();
   }
 
   @override
-  Future<MovieDetail?> fetchMovieDetail(int id) {
-    // TODO: implement fetchMovieDetail
-    throw UnimplementedError();
+  Future<List<MovieDetail>> fetchMovieDetail(int id) async {
+    final result = await _movieDataSource.fetchMovieDetail(id);
+    return result
+        .map(
+          (e) => MovieDetail(
+            genreIds: e.genreIds,
+            id: e.id,
+            originalTitle: e.originalTitle,
+            overview: e.overview,
+            popularity: e.popularity,
+            posterPath: e.posterPath,
+            releaseDate: e.releaseDate,
+            title: e.title,
+            voteAverage: e.voteAverage,
+            voteCount: e.voteCount,
+          ),
+        )
+        .toList();
   }
 }
--- lib/domain/repository/movie_repository.dart
+++ lib/domain/repository/movie_repository.dart
@@ -4,13 +4,13 @@ import 'package:flutter_movie_reservation_app/domain/entity/movie.dart';
 import 'package:flutter_movie_reservation_app/domain/entity/movie_detail.dart';
 
 abstract interface class MovieRepository {
-  Future<List<Movie>?> fetchNowPlayingMovies();
+  Future<List<Movie>> fetchNowPlayingMovies();
 
-  Future<List<Movie>?> fetchPopularMovies();
+  Future<List<Movie>> fetchPopularMovies();
 
-  Future<List<Movie>?> fetchTopRatedMovies();
+  Future<List<Movie>> fetchTopRatedMovies();
 
-  Future<List<Movie>?> fetchUpcomingMovies();
+  Future<List<Movie>> fetchUpcomingMovies();
 
-  Future<MovieDetail?> fetchMovieDetail(int id);
+  Future<List<MovieDetail>> fetchMovieDetail(int id);
 }
