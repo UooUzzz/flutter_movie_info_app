e04342b77f03a056559c7a0daa9ce73f50de8868
UooUzzz <uuumonazzz@gmail.com>
2024-12-27 19:00:11 +0900

    add: movieRemoteDataSourceImpl

--- lib/data/data_source/movie_Remote_data_source_impl.dart
+++ lib/data/data_source/movie_Remote_data_source_impl.dart
@@ -1,4 +1,5 @@
 import 'package:dio/dio.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_movie_reservation_app/data/data_source/movie_data_source.dart';
 import 'package:flutter_movie_reservation_app/data/dto/movie_detail_dto.dart';
 import 'package:flutter_movie_reservation_app/data/dto/movie_response_dto.dart';
@@ -10,33 +11,117 @@ class MovieRemoteDataSourceImpl implements MovieDataSource {
     baseUrl: 'https://api.themoviedb.org/3/movie/',
   ));
 
+  // API 키 .env 갖고오기
+  final String _apiKey = dotenv.env['TMDB_API_KEY'] ?? '';
+
+  // MovieRemoteDataSourceImpl();
+
   @override
-  Future<MovieDetailDto?> fetchMovieDetail(int id) {
-    // TODO: implement fetchMovieDetail
-    throw UnimplementedError();
+  Future<MovieDetailDto?> fetchMovieDetail(int id) async {
+    try {
+      final response = await _dio.get(
+        '$id',
+        queryParameters: {
+          'api_key': _apiKey,
+          'language': 'ko-KR',
+        },
+      );
+      if (response.statusCode == 200) {
+        final data = response.data;
+        return MovieDetailDto.fromJson(data);
+      } else {
+        throw Exception('Failed to load movie details');
+      }
+    } catch (e) {
+      rethrow;
+    }
   }
 
   @override
-  Future<MovieResponseDto?> fetchNowPlayMovies() {
-    // TODO: implement fetchNowPlayMovies
-    throw UnimplementedError();
+  Future<MovieResponseDto?> fetchNowPlayMovies() async {
+    try {
+      final response = await _dio.get(
+        'now_playing',
+        queryParameters: {
+          'api_key': _apiKey,
+          'language': 'ko-KR',
+          'page': 1,
+        },
+      );
+      if (response.statusCode == 200) {
+        final data = response.data;
+        return MovieResponseDto.fromJson(data);
+      } else {
+        throw Exception('Failed to load now playing movies');
+      }
+    } catch (e) {
+      rethrow;
+    }
   }
 
   @override
-  Future<MovieResponseDto?> fetchPopularMovies() {
-    // TODO: implement fetchPopularMovies
-    throw UnimplementedError();
+  Future<MovieResponseDto?> fetchPopularMovies() async {
+    try {
+      final response = await _dio.get(
+        'popular',
+        queryParameters: {
+          'api_key': _apiKey,
+          'language': 'ko-KR',
+          'page': 1,
+        },
+      );
+      if (response.statusCode == 200) {
+        final data = response.data;
+        return MovieResponseDto.fromJson(data);
+      } else {
+        throw Exception('Failed to load popular movies');
+      }
+    } catch (e) {
+      rethrow;
+    }
   }
 
   @override
-  Future<MovieResponseDto?> fetchTopRatedMovies() {
-    // TODO: implement fetchTopRatedMovies
-    throw UnimplementedError();
+  Future<MovieResponseDto?> fetchTopRatedMovies() async {
+    try {
+      final response = await _dio.get(
+        'top_rated',
+        queryParameters: {
+          'api_key': _apiKey,
+          'language': 'ko-KR',
+          'page': 1,
+        },
+      );
+      if (response.statusCode == 200) {
+        final data = response.data;
+        return MovieResponseDto.fromJson(data);
+      } else {
+        throw Exception('Failed to load top rated movies');
+      }
+    } catch (e) {
+      rethrow;
+    }
   }
 
   @override
-  Future<MovieResponseDto?> fetchUpcomingMovies() {
-    // TODO: implement fetchUpcomingMovies
-    throw UnimplementedError();
+  Future<MovieResponseDto?> fetchUpcomingMovies() async {
+    try {
+      final response = await _dio.get(
+        'upcoming',
+        queryParameters: {
+          'api_key': _apiKey,
+          'language': 'ko-KR',
+          'page': 1,
+        },
+      );
+      if (response.statusCode == 200) {
+        final data = response.data;
+        return MovieResponseDto.fromJson(data);
+      } else {
+        throw Exception('Failed to load upcoming movies');
+      }
+    } catch (e) {
+      rethrow;
+    }
   }
 }
