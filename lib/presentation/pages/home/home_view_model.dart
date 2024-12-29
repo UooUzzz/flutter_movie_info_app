88345c45f7566247eeed905631a53aefe1316687
UooUzzz <uuumonazzz@gmail.com>
2024-12-29 15:55:22 +0900

    fix: viewmodel

--- lib/presentation/pages/home/home_view_model.dart
+++ lib/presentation/pages/home/home_view_model.dart
@@ -5,29 +5,73 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 // 1. 상태클래스
 // 영화 리스트 List<Movie>
 
-class HomeViewModel extends Notifier<List<Movie>> {
+// 카테고리 별로 뷰모델 생성
+
+// class HomeViewModel extends Notifier<List<Movie>> {
+//   @override
+//   List<Movie> build() => [];
+
+//   Future<void> fetchNowPlayingMovies() async {
+//     final fetchMovieUsecase = ref.read(fetchMovieUsecaseProvider);
+//     final result = await fetchMovieUsecase.executeNowPlaying();
+//     state = result;
+//   }
+
+//   Future<void> fetchPopularMovies() async {
+//     final fetchMovieUsecase = ref.read(fetchMovieUsecaseProvider);
+//     final result = await fetchMovieUsecase.executePopular();
+//     state = result;
+//   }
+
+//   Future<void> fetchTopRatedMovies() async {
+//     final fetchMovieUsecase = ref.read(fetchMovieUsecaseProvider);
+//     final result = await fetchMovieUsecase.executeTopRated();
+//     state = result;
+//   }
+
+//   Future<void> fetchUpcomingMovies() async {
+//     final fetchMovieUsecase = ref.read(fetchMovieUsecaseProvider);
+//     final result = await fetchMovieUsecase.executeUpcoming();
+//     state = result;
+//   }
+// }
+
+class NowPlayingViewModel extends Notifier<List<Movie>> {
   @override
-  List<Movie> build() {
-    return [];
-  }
+  List<Movie> build() => [];
 
   Future<void> fetchNowPlayingMovies() async {
     final fetchMovieUsecase = ref.read(fetchMovieUsecaseProvider);
     final result = await fetchMovieUsecase.executeNowPlaying();
     state = result;
   }
+}
 
-  Future<void> fetchPopularMovies() async {
+class PopularViewModel extends Notifier<List<Movie>> {
+  @override
+  List<Movie> build() => [];
+
+  Future<void> fetchPopulargMovies() async {
     final fetchMovieUsecase = ref.read(fetchMovieUsecaseProvider);
     final result = await fetchMovieUsecase.executePopular();
     state = result;
   }
+}
+
+class TopRatedViewModel extends Notifier<List<Movie>> {
+  @override
+  List<Movie> build() => [];
 
   Future<void> fetchTopRatedMovies() async {
     final fetchMovieUsecase = ref.read(fetchMovieUsecaseProvider);
     final result = await fetchMovieUsecase.executeTopRated();
     state = result;
   }
+}
+
+class UpcomingViewModel extends Notifier<List<Movie>> {
+  @override
+  List<Movie> build() => [];
 
   Future<void> fetchUpcomingMovies() async {
     final fetchMovieUsecase = ref.read(fetchMovieUsecaseProvider);
@@ -36,8 +80,23 @@ class HomeViewModel extends Notifier<List<Movie>> {
   }
 }
 
-final homeViewModelProvider = NotifierProvider<HomeViewModel, List<Movie>>(
-  () {
-    return HomeViewModel();
-  }
-);
+// final homeViewModelProvider = NotifierProvider<HomeViewModel, List<Movie>>(() {
+//   return HomeViewModel();
+// });
+
+// 카테고리별 뷰모델 공급자
+final nowPlayingViewModelProvider = NotifierProvider<NowPlayingViewModel, List<Movie>>(() {
+  return NowPlayingViewModel();
+});
+
+final popularViewModelProvider = NotifierProvider<PopularViewModel, List<Movie>>(() {
+  return PopularViewModel();
+});
+
+final topRatedViewModelProvider = NotifierProvider<TopRatedViewModel, List<Movie>>(() {
+  return TopRatedViewModel();
+});
+
+final upcomingViewModelProvider = NotifierProvider<UpcomingViewModel, List<Movie>>(() {
+  return UpcomingViewModel();
+});
